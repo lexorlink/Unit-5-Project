9278b79f78b70113b1ccacbe856fbbe5e8f82fe5
IplayBotw <legendsofleon72@gmail.com>
2025-12-18 10:08:01 -0500

    Update Unit5Project.cpp
    Main while loop implemented. Feel free to edit for however we end up getting input.

--- Unit5Project.cpp
+++ Unit5Project.cpp
@@ -6,5 +6,21 @@
 using namespace std;
 
 int main(){
-  
+    bool programActive = true;
+    string userInput;
+    while(programActive == true){
+        //Wealcomes user
+        cout<<"------ Welcome to Word Counter ------"<<endl;
+        cout<<"Please enter your document or enter 'exit' to end program"<<endl;
+        //user input not implemented
+        cin>>userInput;
+        //exits the loop
+        if (userInput == "exit" || userInput == "Exit"){
+            cout<<"Ending program ...";
+            programActive = false;
+        }
+        else{
+            //This is where we should get the input for word count
+        }
+    }
 }
