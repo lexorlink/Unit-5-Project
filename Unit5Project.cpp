451bc14746872be292dfd649b97117fadaa7b359
IplayBotw <legendsofleon72@gmail.com>
2026-01-14 10:39:12 -0500

    Infinite print bug fixes, additional user information

--- Unit5Project.cpp
+++ Unit5Project.cpp
@@ -4,27 +4,34 @@
 #include <string>
 #include <iomanip>
 using namespace std;
-
+bool openWelcomeWindow = true;
+bool programActive = true;
 int main(){
-    bool programActive = true;
-    string userInput;
     string userFilePath;
     while(programActive = true){
         //Welcomes user
-        cout<<"------ Welcome to Word Counter ------"<<endl;
-        cout<<"Please copy in your file path or enter 'exit' to end program"<<endl;
-        cin>>userFilePath;
+        if (openWelcomeWindow == true){
+            cout<<"----------Welcome to Word Counter----------"<<endl;
+            cout<<"Enter your file path or type 'exit' to close the program (preferably a .txt file)"<<endl;
+            cout<<"Note that the file name must NOT contain any spaces"<<endl;
+            cin>>userFilePath;
+            openWelcomeWindow = false;
+            //disables the window from being reopened until needed
+        }
 
         if (userFilePath == "exit" || userFilePath == "Exit"){
             cout<<"Ending program ...";
-            programActive = false;
+            break;
             //exits the loop
         }
         if (userFilePath != "exit" || userFilePath != "Exit"){
+            cout<<"The path you have selected is: "<<userFilePath << endl;
             ofstream inFile(userFilePath); 
-            programActive = false;
-            //this just lets us confirm the file works
+            
+            break;
+            //this just lets us confirm the file works, data scanning needs implementation
             
         }
     }
 }
+
