18ceec4ab745f2a5b36b61823837b3c3510dc3ce
IcyBoom <Trruden320@gmail.com>
2026-02-11 23:28:28 -0500

    Update Unit5Project.cpp

--- Unit5Project.cpp
+++ Unit5Project.cpp
@@ -7,20 +7,24 @@ using namespace std;
 //C:\\Users\\89173\\U-5-text.txt 
 
 map<char, int> counter;
-char characterCounter;
-string userFilePath, line, start;
-int wordCount = 0, toolChoice;
+
+void charCounter(ifstream& inFile);
+void wordCounter(ifstream& inFile);
+void textOutput(ifstream& inFile);
 
 int main() {
+
+    string userFilePath, start;
+    int toolChoice, rerunNum;
 
     cout << "----------Welcome to Word Counter----------" << endl;
     cout << "Enter 'start' to begin the program" << endl;
     cin >> start;
-    
-    
-    do {
+
+    while (start == "start"){
 
         cout << "Enter your file path or type 'exit' to close the program" << endl;
+        cin.ignore();
         getline(cin, userFilePath);
 
         if (userFilePath == "exit" || userFilePath == "Exit") {
@@ -31,7 +35,7 @@ int main() {
         ifstream inFile(userFilePath);
         if (!inFile) {
             cout << "Failed to open file." << endl;
-            break;
+            continue;
         }
         
         cout << "Please choose which tool you would like to use, 1: Basic Word Counter, 2: Character Counter, 3: Specific Character Counter, 4: All Tools."<<endl;
@@ -40,34 +44,44 @@ int main() {
 
             switch (toolChoice){
                 case 1:
-                wordCounter();
-                textOutput();
+                wordCounter(inFile);
+                textOutput(inFile);
                 break;
                 case 2:
-                charCounter();
-                textOutput();
+                charCounter(inFile);
+                textOutput(inFile);
                 break;
                 case 3:
-                textOutput();
+                textOutput(inFile);
                 break;
                 case 4:
-                wordCounter();
-                charCounter();
-                textOutput();
+                wordCounter(inFile);
+                charCounter(inFile);
+                textOutput(inFile);
                 break;
                 default:
                     cout << "You entered an invalid number, please try again" << endl;
                 break;
             }
 
-    } while (start == start);
+            cout << "Please enter either, '1' to use anouther tool or, '2' to exit the program." <<endl;
+            cin >> rerunNum;
 
-    inFile.close();
+            if (rerunNum == 2){
+                start = "exit";
+            }
+    
+            inFile.close();
+
+        }
 
     return 0;
 
 }
-void charCounter(){
+void charCounter(ifstream& inFile){
+    
+    counter.clear();
+    char characterCounter;
     int totalCharacters = 0;
 
     while (inFile.get(characterCounter)) {
@@ -75,27 +89,37 @@ void charCounter(){
         totalCharacters++;
     }
     cout << "This document contains: " << totalCharacters << " characters." << endl;
+    
     inFile.clear();
     inFile.seekg(0);
 }
 
-void wordCounter(){
+void wordCounter(ifstream& inFile){
+    
+    int wordCount = 0; 
     string word;
 
     while (inFile >> word){
         wordCount++;
     }
     cout << "This document contains: " << wordCount << " words" << endl;
+    
     inFile.clear();
     inFile.seekg(0);
 }
-void textOutput(){
+void textOutput(ifstream& inFile){
+
+    string line;
+
     cout << "\n----- File Contents -----" << endl;
 
-        while (getline(inFile, line)) {
-            cout << line << endl;
-        }
+    while (getline(inFile, line)) {
+        cout << line << endl;
+    }
 
-        cout << "-------------------------" << endl;
+    cout << "-------------------------" << endl;
+
+    inFile.clear();
+    inFile.seekg(0);
 
 }
